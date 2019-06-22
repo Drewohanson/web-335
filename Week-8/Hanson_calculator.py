359d12ed2e3147a8656ff112a47c8537ad2d4f2a
Drew Hanson <drew.o.hanson@gmail.com>
2019-06-22 00:06:45 -0600

    adding 8.3

---
+++ Week-8/Hanson_calculator.py
@@ -0,0 +1,25 @@
+# ============================================
+# ; Title:  Hanson_calculator.py
+# ; Author: Professor Krasso
+# ; Date:  21 June 2019
+# ; Modified By: Drew Hanson
+# ; Description: Calculator functions
+# ;===========================================
+
+first_name = "Drew"
+last_name = "Hanson"
+
+def add(first, second):
+    return first + second
+
+def subtract(first, second):
+    return first - second
+
+def divide(first, second):
+    return first / second
+
+print(first_name + " " + last_name)
+print(' ')
+print(add(1, 4))
+print(subtract(7, 2))
+print(divide(9, 3))
