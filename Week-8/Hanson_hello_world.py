53a68898e6d2bf82e14fa40a63ef743e65e7ae03
Drew Hanson <drew.o.hanson@gmail.com>
2019-06-21 23:56:28 -0600

    week 8

---
+++ Week-8/Hanson_hello_world.py
@@ -0,0 +1,9 @@
+# ============================================
+# ; Title:  Hanson_hello_world.py
+# ; Author: Professor Krasso
+# ; Date:  21 June 2019
+# ; Modified By: Drew Hanson
+# ; Description: Hello World
+# ;===========================================
+
+print("Hello World")
